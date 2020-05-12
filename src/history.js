ee34430dfb8e421b41e237b3a312351982066248
ngockiem <ngockiemit92@gmail.com>
2020-05-12 22:24:19 +0700

    Initial commit

---
+++ src/history.js
@@ -0,0 +1,37 @@
+/*
+(1) Cài đặt module:
+- react-router-doom
+- react-icons
+
+(2) Thêm các file mẫu vào:
+https://github.com/john-smilga/setup-files-react-beach-resort
+
+(3) Cài đặt redirect
+tạo một file _redirect trong thư mục [src]
+nội dung: 
+---------- _redirect ----------
+/* /index.html 200
+
+(4) Tạo thư mục [components] để chứa các component
+- Home
+- Rooms
+- SingleRoom
+- Error
+
+(5) Cài đặt Router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
